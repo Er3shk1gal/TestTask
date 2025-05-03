925764816998d86e5f3dc3cd38a0dc701c80a017
Ereshkigal <roma.hlinin@yandex.ru>
2025-05-03 16:05:26 +0300

    feat: Реализовал класс сервер с необходимыми блокировками

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Task2;
+
+var cts = new CancellationTokenSource();
+var token = cts.Token;
+
+for (int i = 0; i < 3; i++)
+{
+    Task.Run(() =>
+    {
+        while (!token.IsCancellationRequested)
+        {
+            Console.WriteLine($"Читатель {Task.CurrentId}: сумма = {Server.GetCount()}");
+            Thread.Sleep(500);
+        }
+    }, token);
+}
+
+for (int i = 0; i < 2; i++)
+{
+    Task.Run(() =>
+    {
+        var random = new Random();
+        while (!token.IsCancellationRequested)
+        {
+            int value = random.Next(1, 5);
+            Server.AddToCount(value);
+            Console.WriteLine($"Писатель {Task.CurrentId}: добавил {value}");
+            Thread.Sleep(1500);
+        }
+    }, token);
+}
+
+Console.ReadLine();
+cts.Cancel();
